7930b29279df371c104128b1692c0ce715c395c7
guotai <guotaixiao87@gmail.com>
2025-05-16 20:56:32 +1000

    cror

--- server.js
+++ server.js
@@ -19,6 +19,13 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Methods',
     'GET, POST, PATCH, DELETE, OPTIONS'
   );
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  // Handle OPTIONS preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
   next();
 });
 
